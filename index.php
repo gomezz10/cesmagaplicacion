929d01efa374ec2eb2ad87d55f408b7ee6d907d5
Yeferson <cordobayef227@gmail.com>
2023-10-25 08:50:26 -0500

    cuarta version

--- index.php
+++ index.php
@@ -6,9 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="css/mdb.min.css">
+<script src="js/mdb.min.js"></script>
 <body>
     <h1>Cesmag app</h1>
-    <h2>Nombre yeferson cordoba</h2>
-    <h3>carrera: Ingenieria en sistemas</h3>
+    
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">cesmag app</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="Cesmag app">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Facultades</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Programnas</a>
+        </li>
+        <li class="nav-item">
+          
+           
+          
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </body>
 </html>
